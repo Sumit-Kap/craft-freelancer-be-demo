f88d0a21119058747f808a045063618a7f1710c4
sumit-kap <smtkpr88@gmail.com>
2024-09-19 01:24:21 +0530

    add be demo-users

--- models/User.js
+++ models/User.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
+    bio: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    skills: { type: [String], required: true },
+    githubProfile: { type: String, required: true },
+    salary: { type: Number, required: true },
     email: { type: String, required: true, index: true, unique: true },
-    role: { type: String, required: true },
-    userId: { type: String, required: true, index: true, unique: true },
-    tags: { type: [String], required: true },
-    githubUserName: { type: String, required: true },
   },
   { timestamps: true }
 );
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 dbConfig.connect();
 const Jobs = require("./models/Job");
-
+const Users = require("./models/User");
 app.get("/api/v1/health", (req, res) => {
   res.send("<h1>Server up and running</h1>");
 });
@@ -54,6 +54,42 @@ app.get("/api/v1/fetchJobs/:id", async (req, res) => {
   console.log("print", req.params);
   try {
     const response = await Jobs.find({ employerId: id });
+    res.status(200).json({ status: "ok", data: response });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ code: "Error", message: err });
+  }
+});
+
+app.post("/api/v1/createUser", async (req, res) => {
+  const { bio, contactNumber, skills, salary, email, gitHubProfile } = req.body;
+  const user = new Users({
+    bio: bio,
+    contactNumber: contactNumber,
+    salary: salary,
+    email: email,
+    skills: skills.split(","),
+    githubProfile: gitHubProfile,
+  });
+
+  user
+    .save()
+    .then((response) => {
+      res
+        .status(200)
+        .json({ code: "ok", message: "record saved successfully" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ code: "Error", message: err });
+    });
+});
+
+app.get("/api/v1/fetchUser/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log("print", req.params);
+  try {
+    const response = await Jobs.find({ email: id });
     res.status(200).json({ status: "ok", data: response });
   } catch (err) {
     console.log(err);
